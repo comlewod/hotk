d8bf8f73a9e6cf26ba4079f608de859e036f5b90
comlewod <comlewod@163.com>
2017-08-03 18:36:02 +0800

    connect database

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var mysql = require('mysql');
+var db = require('./conf/db');
+var os = require('os');
 
 var app = new express();
 
+//创建连接池，提高性能
+var pool = mysql.createPool(db.mysql);
+
 app.get('/', function(req, res){
-	res.send('Hello, Athena');
+	pool.getConnection(function(err, connection){
+		connection.query('SELECT * FROM user', function(err, results, fields){
+			if( err ) throw err;
+			res.json(results);
+		});
+	});
+	//res.send('Hello, Athena');
 });
 
-app.listen(3000);
+module.exports = app;
---
+++ conf/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+	mysql: {
+		host: '127.0.0.1',
+		user: 'root',
+		password: '123123',
+		database: 'hotk',
+		port: 3306
+	}
+};
