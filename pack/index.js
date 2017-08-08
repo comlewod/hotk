5b83ea5bbe576a5843d1cdadf2b6673e60e5e8d6
comlewod <comlewod@163.com>
2017-08-08 18:39:12 +0800

    pack image

--- pack/index.js
+++ pack/index.js
@@ -1,12 +1,38 @@
 var config = require('./config');
-var fs = require('fs');
+var packImage = require('./packImage');
 
-console.log(config.pages);
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
+var uglifyJs = require('uglify-js');
 
 var content = '';
-config.pages.forEach(file => {
-	content += fs.readFileSync(file, {encoding: 'utf8'});
+config.pages.forEach(filePath => {
+	var info = pageInfo(filePath);
+
+	info.widgets.forEach(widget => {
+		var file_js = path.join(config.views, info.name, widget, '*.js');
+	});
+
+	content += fs.readFileSync(filePath, {encoding: 'utf8'});
 });
 
-function concatContent(file){
+//获取页面组件等信息
+function pageInfo(filePath){
+	var arr = filePath.split('/').reverse();
+	var pagePath = path.join(config.views, arr[1], '*', '*.html');
+	
+	var widgets = [];
+	var widgetArr = glob.sync(pagePath);
+	widgetArr.forEach(_path => {
+		var _arr = _path.split('/').reverse();
+		widgets.push(_arr[1]);
+	});
+
+	return {
+		name: arr[1],		//页面名称
+		file: arr[0],		//页面入口文件
+		widgets: widgets	//页面所含有的组件名称
+	};
 }
+
---
+++ pack/packImage.js
@@ -0,0 +1,6 @@
+var imagemin = require('imagemin');
+
+function packImage(){
+}
+
+module.exports = packImage;
