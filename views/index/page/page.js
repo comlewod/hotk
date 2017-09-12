015086b92bfef8b595c55f3acb5cb7f669240e00
comlewod <comlewod@163.com>
2017-09-12 18:20:41 +0800

    upload img

--- views/index/page/page.js
+++ views/index/page/page.js
@@ -1,10 +1,28 @@
 $(function(){
 	//var myDrop = new Dropzone('#j-upload', {
-	$('#j-upload').dropzone({
+	var mydrop = new Dropzone('#j-upload', {
 		url: '/upload',
 		paramName: function(){
 			return 'test';
 		},
-		uploadMultiple: true
+		uploadMultiple: true,		//是否支持多图上传
+		autoProcessQueue: false,	//设置为false的话就需要手动上传图片，通过mydrop.processQueue()
+		uploadprogress: function(a, b, c){
+			console.log(a, b, c);
+		},
+		queuecomplete: function(){	//上传完成后 
+			console.log(123);
+		},
+		previewTemplate: '\
+		<div class="dz-preview">\
+			<img data-dz-thumbnail />\
+			<div class="dz-process">\
+				<span class="process" data-dz-uploadprogress></span>\
+			</div>\
+		</div>\
+		',
+	});
+	$('#upload').on('click', function(){
+		mydrop.processQueue();
 	});
 });
