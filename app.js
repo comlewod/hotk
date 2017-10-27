7b41531b64ed1460413d7abe2137fad0c565d4d1
comlewod <comlewod@163.com>
2017-10-27 18:08:30 +0800

    test

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var mysql = require('mysql');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var timeout = require('connect-timeout');
 var controller = require('./controller/init');
 var os = require('os');
 
 var app = new express();
 
+app.use(timeout('3s'));
 app.engine('.html', require('ejs').__express);
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'html');
@@ -20,11 +22,19 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
+
+app.use(haltOnTimeout);
 app.use(controller);//通过express的router来获取所设置的路由
+app.use(haltOnTimeout);
+
+function haltOnTimeout(req, res, next){
+	if( !req.timedout ) next();
+}
 
 //错误处理
 app.use((err, req, res, next) => {
-	res.send('wrong');
+	console.log(123);
+	res.send(err.message);
 });
 
 module.exports = app;
