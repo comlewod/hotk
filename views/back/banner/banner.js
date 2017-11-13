b1a6de63a362568e91f64df8677c227a587aa3a6
comlewod <comlewod@163.com>
2017-11-13 17:53:19 +0800

    back window

--- views/back/banner/banner.js
+++ views/back/banner/banner.js
@@ -1,5 +1,24 @@
 G_Module.define('banner', {
 	initialize: function(opts){
-		this.opts = opts;
+		this.initVue(opts);
 	},
+	initVue: function(opts){
+		new Vue({
+			el: '.j-main-page',
+			data: {
+				title: 'Add Banner',
+				winShow: false,
+			},
+			methods: {
+				chooseImg: function(){
+					console.log(123);
+				},
+				uploadImg: function(){
+				},
+				winHide: function(){
+					this.winShow = false;
+				},
+			}
+		});
+	}
 });
--- views/back/window/window.js
+++ views/back/window/window.js
@@ -1,5 +1,24 @@
 G_Module.define('window', {
 	initialize: function(opts){
 		this.opts = opts;
+		this.initComponent();
+	},
+	initComponent: function(){
+		Vue.component('comp-window', {
+			template: '#comp-window',
+			props: {
+				title: {type: String, default: ''},
+				show: false
+			},
+			data: function(){
+				return {
+				};
+			},
+			methods: {
+				close: function(){
+					this.$emit('cancel');
+				},
+			},
+		});
 	},
 });
